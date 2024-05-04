49b161cc6057262d9a4dc1c518261429d46d5d24
Ashick Sheriff <105576514+AshickSheriff@users.noreply.github.com>
2024-05-04 23:14:14 +0530

    Aq 04 (#4)
    * aq-04 routes added
    
    * aq-04 routes added 1

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,1 @@
-redis_config = Rails.application.credentials.redis[Rails.env.to_sym]
-Redis.current = Redis.new(
-  url: redis_config[:url],
-  port: redis_config[:port],
-  db: redis_config[:db]
-)
+\
