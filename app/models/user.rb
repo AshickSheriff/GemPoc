fafff8afeb8743990fcbc21c9a1fe782b2efd4ac
AshickSheriff <asiqsherieff@gmail.com>
2024-05-05 09:38:07 +0530

    [cc-test] cc testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def name
+  end
+
+  def name
+  end
 end
